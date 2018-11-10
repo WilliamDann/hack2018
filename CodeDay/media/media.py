4039edf40d5417c361a8e6d2c0888eaffc37e5c3
WilliamDann <wcdann1@gmail.com>
2018-11-10 14:55:27 -0800

    Added media class

---
+++ CodeDay/media/media.py
@@ -0,0 +1,7 @@
+# General media object
+class MediaObject:
+    def __init__(self, title, image, link, content):
+        self.title = title
+        self.image = image
+        self.link = link
+        self.content = content
