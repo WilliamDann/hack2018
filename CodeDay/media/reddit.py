d38f80682a4469746ae0d91d77204e2d78e2470a
WilliamDann <wcdann1@gmail.com>
2018-11-10 15:42:38 -0800

    Added reddit support

---
+++ CodeDay/media/reddit.py
@@ -0,0 +1,38 @@
+import praw
+import media
+
+instance = None
+
+# Initilize a connection with reddit
+def init():
+    global instance
+    instance = praw.Reddit(
+        client_id="nDBkcRjDgJIqIQ",
+        client_secret="I6O4uV0jRaNkOmVjD4bzAqS3Iq0",
+        user_agent="makemehappy bot",
+        username="makemehappybot",
+        password="makemehappybot"
+    )
+
+# Get the posts from a subreddit
+def getPosts(subreddit, limit=20, source='hot'):
+    if instance is None:
+        raise Exception("Reddit instance not initilized! call init()")
+
+    subredditItem = instance.subreddit(subreddit)
+    
+    data = None
+    returnData = []
+
+    # TODO add all data sources
+    if (source == 'hot'):
+        data = subredditItem.hot(limit=limit)
+    elif (source == 'top'):
+        data = subredditItem.top(limit=limit)
+
+    for post in data:
+        returnData.append(media.MediaObject(post.title, post.media, post.shortlink))
+    
+    return returnData
+
+init()
