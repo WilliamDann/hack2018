dac71c1712ad03638ae2fa060128b93f01cebed5
Ottomated <otto@ottomated.pw>
2018-11-10 23:57:59 -0800

    CSS/JS

--- CodeDay/static/js/index.js
+++ CodeDay/static/js/index.js
@@ -1,5 +1,15 @@
 const button = document.getElementById('main-button');
 
+const content = {
+    quote: document.getElementById('quote'),
+    image: document.getElementById('image'),
+    imageTitle: document.getElementById('image-title'),
+    imageUrl: document.getElementById('image-url'),
+    newsTitle: document.getElementById('news-title'),
+    newsDesc: document.getElementById('news-desc'),
+    newsUrl: document.getElementById('news-url'),
+};
+
 button.addEventListener('click', () => {
     setTimeout(() => {
         button.innerHTML = 'Make More Happy';
@@ -7,6 +17,12 @@ button.addEventListener('click', () => {
     fetch('/posts').then((res) => {
         res.json().then((json) => {
             console.log(json);
+            content.image.href = json.imageurl;
+            content.imageTitle.innerHTML = json.imagetitle;
+            content.quote.innerHTML = json.quote;
+            content.newsTitle.innerText = json.newstitle;
+            content.newsDesc.innerText = json.newsdesc;
+            content.newsUrl.href = json.newsurl;
         });
     })
 });
@@ -24,3 +40,8 @@ let updateNight = () => {
 updateNight();
 
 setInterval(updateNight, 5000);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, {});
+  });
