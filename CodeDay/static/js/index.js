7f4e025e19a74462542b390dcf7f4471cd296d10
Ottomated <otto@ottomated.pw>
2018-11-11 07:04:31 -0800

    Finalize frontent

--- CodeDay/static/js/index.js
+++ CodeDay/static/js/index.js
@@ -1,4 +1,5 @@
 const button = document.getElementById('main-button');
+const mainbody = document.getElementById('mainbody');
 
 const content = {
     quote: document.getElementById('quote'),
@@ -8,47 +9,41 @@ const content = {
     newsTitle: document.getElementById('news-title'),
     newsDesc: document.getElementById('news-desc'),
     newsUrl: document.getElementById('news-url'),
+    newsImage: document.getElementById('news-image'),
 };
 
 button.addEventListener('click', () => {
     button.classList.add('focused');
+    mainbody.style.transform = 'translateY(-86%)';
+    setTimeout(() => {
+
+        content.newsTitle.innerText = '';
+        content.newsDesc.innerText = '';
+        content.imageTitle.innerHTML = '';
+    }, 400);
     setTimeout(() => {
         button.innerHTML = 'Make More Happy';
     }, 250);
-    //fetch('/posts').then((res) => {
-        //res.json().then((json) => {
-            json = {
-                "imagetitle": "I met a homeless guy on the pier at Venice Beach today. He was telling jokes for donations. I gave him $20 to tell me his three best. I was not disappointed nor will you.",
-                "imageurl": {
-                  "reddit_video": {
-                    "dash_url": "https://v.redd.it/6zz13u4kqlx11/DASHPlaylist.mpd",
-                    "duration": 48,
-                    "fallback_url": "https://v.redd.it/6zz13u4kqlx11/DASH_9_6_M",
-                    "height": 1080,
-                    "hls_url": "https://v.redd.it/6zz13u4kqlx11/HLSPlaylist.m3u8",
-                    "is_gif": false,
-                    "scrubber_media_url": "https://v.redd.it/6zz13u4kqlx11/DASH_600_K",
-                    "transcoding_status": "completed",
-                    "width": 608
-                  }
-                },
-                "newsdesc": "An American trucker passing through Ottawa treated a busload of Canadian veterans to dinner to honour their service.",
-                "newstitle": "American trucker treats busload of Canadian veterans to dinner",
-                "newsurl": "https://beta.ctvnews.ca/content/ctvnews/en/national/canada/2018/11/10/1_4171268.html",
-                "quote": "30+ years as renters and my parents have finally bought a house!"
-              };
+    fetch('/posts').then((res) => {
+        res.json().then((json) => {
             console.log(json);
-            content.image.href = json.imageurl;
-            content.imageTitle.innerHTML = json.imagetitle;
-            content.quote.innerHTML = json.quote;
-            content.newsTitle.innerText = json.newstitle;
-            content.newsDesc.innerText = json.newsdesc;
-            content.newsUrl.href = json.newsurl;
+            content.quote.innerHTML = json.quote.text;
+
+            let r = /https:\/\/imgur.com\/(.+)/;
+            if (r.test(json.image.image))
+                json.image.image = r.exec(json.image.image)[1];
+            content.image.src = json.image.image;
+            content.imageTitle.innerHTML = json.image.title;
+            
+            content.newsTitle.innerText = json.news.title;
+            content.newsDesc.innerText = json.news.description;
+            content.newsImage.src = json.news.image;
             button.classList.remove('focused');
-        //});
-    //}).catch(e => {
-        //button.classList.remove('focused');
-    //});
+            mainbody.style.transform = 'translateY(0)';
+        });
+    }).catch(e => {
+        button.classList.remove('focused');
+    });
 });
 let updateNight = () => {
     let t = new Date();
@@ -58,7 +53,6 @@ let updateNight = () => {
     } else {
         if (document.body.className === 'night')
             document.body.className = '';
-
     }
 };
 updateNight();
